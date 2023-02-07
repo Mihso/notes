b8bbbc5afb21024c93f5179115267e2be796be58
Matthew Oshimo <moshimo90696@gmail.com>
2023-02-06 23:21:55 -0800

    implement typescript to frontend

---
+++ services/functions/test.ts
@@ -0,0 +1,17 @@
+import { router, publicProcedure } from './trpc';
+import { z } from 'zod';
+ 
+export const appRouter = router({
+  // Create publicProcedure at path 'hello'
+  hello: publicProcedure.input(
+    z.object({
+        title: z.string(),
+        url: z.string(),}))
+        .query(({input}) => {
+    return {
+      greeting: `hello world ${input?.title} ${input?.url} ?? 'world'`,
+    };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
--- services/server/client.ts
+++ services/server/client.ts
@@ -2,6 +2,12 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from './server';
  
 // Notice the <AppRouter> generic here.
+const user = await trpc.userById.query('1');
+user.id;
+user.name;
+const createdUser = await trpc.userCreate.mutate({name : 'sachinraja'});
+createdUser.id;
+createdUser.name;
 const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
